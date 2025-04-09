254bdc4b0f7143fbcab72655951f138bdc196881
Vaigunda Raja <miniproject172@gmail.com>
2025-04-09 09:13:13 +0530

    qiuck

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
+    SECRET_KEY = os.environ.get('raja') or 'your-secret-key'
     SQLALCHEMY_DATABASE_URI = 'postgresql://quick_aid_z1m4_user:WWKP1FQUA1j0kGt6t0DxobF1Saz9uY1U@dpg-cvllce8gjchc738junp0-a.oregon-postgres.render.com/quick_aid_z1m4'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
