36362fa87a880fc1d3450047724846ab5daefad6
Vaigunda Raja <miniproject172@gmail.com>
2025-04-09 09:58:10 +0530

    raja

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, redirect, url_for, flash, request
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 from app import app, db
 from app.models import User, Details
+from utils import generate_uid
 from werkzeug.security import generate_password_hash, check_password_hash
 import segno
 import os
